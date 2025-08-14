283eca7ddd6c9b1e1bc92e3c6723081129b1c0c8
minhtai-ferb <minhtai7112003@gmail.com>
2025-08-15 03:34:39 +0700

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,7 +43,7 @@ export async function middleware(req: NextRequest) {
   // Chưa đăng nhập / không đọc được roles
   if (!roles) {
     const url = req.nextUrl.clone();
-    url.pathname = "/auth/login";
+    url.pathname = "/auth";
     return NextResponse.redirect(url);
   }
 
