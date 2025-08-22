9f83b1fd8fd8835a33cb797e6b96d9bdd01574d4
minhtai-ferb <minhtai7112003@gmail.com>
2025-08-22 14:07:12 +0700

    hot fix

--- src/app/(manage)/admin/tour/[id]/page.tsx
+++ src/app/(manage)/admin/tour/[id]/page.tsx
@@ -1,20 +1,18 @@
+"use client";
+
 import TourDetailClient from "@/app/(manage)/components/tour-management/TourDetailClient";
 import BreadcrumbHeader, { Crumb } from "@/components/common/breadcrumb-header";
 import { SidebarInset } from "@/components/ui/sidebar";
-
-interface TourDetailPageProps {
-  params: {
-    id: string;
-  };
-}
+import { useParams } from "next/navigation";
 
 const crumbs: Crumb[] = [
   { label: "Quản lý chuyến tham quan", href: "/admin/tour" },
   { label: "Chi tiết chuyến tham quan" },
 ];
 
-export default function TourDetailPage({ params }: TourDetailPageProps) {
-  const { id } = params;
+export default function TourDetailPage() {
+  const params = useParams();
+  const id = params.id as string;
   return (
     <SidebarInset>
       <BreadcrumbHeader items={crumbs} />
